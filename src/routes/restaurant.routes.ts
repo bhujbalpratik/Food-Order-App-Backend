23ecea28bb843ddf0c957d470c76e6ea6ae61d14
bhujbalpratik <pratikbhujbal743@gmail.com>
2024-06-07 18:36:49 +0530

    update Restaurant API created

--- src/routes/restaurant.routes.ts
+++ src/routes/restaurant.routes.ts
@@ -3,6 +3,7 @@ import multer from "multer"
 import {
   createRestaurant,
   getRestaurant,
+  updateRestaurant,
 } from "../controllers/restaurant.controller"
 import { jwtCheck, jwtParse } from "../middlewares/auth"
 import { validateMyRestaurantRequest } from "../middlewares/validation"
@@ -14,14 +15,23 @@ const upload = multer({
   storage: storage,
   limits: { fileSize: 5 * 1024 * 1024 },
 })
-router.post(
-  "/",
-  upload.single("imageFile"),
-  validateMyRestaurantRequest,
-  jwtCheck,
-  jwtParse,
-  createRestaurant
-)
-router.get("/", jwtCheck, jwtParse, getRestaurant)
+
+router
+  .route("/")
+  .post(
+    upload.single("imageFile"),
+    validateMyRestaurantRequest,
+    jwtCheck,
+    jwtParse,
+    createRestaurant
+  )
+  .get(jwtCheck, jwtParse, getRestaurant)
+  .put(
+    upload.single("imageFile"),
+    validateMyRestaurantRequest,
+    jwtCheck,
+    jwtParse,
+    updateRestaurant
+  )
 
 export default router
